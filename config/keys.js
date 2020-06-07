40fe658fa390d025b3b872ddafb6d769a4125f5c
Joshua K Bader <jaybshinsen@hotmail.com>
2020-06-07 15:09:33 +0800

    updated googleKey

--- config/keys.js
+++ config/keys.js
@@ -2,8 +2,8 @@
 
 module.exports = {
     google: {
-        clientID: '339055223599-fuvqrag4o5n0aql4q4kj67kb0cs9jkfp.apps.googleusercontent.com',
-        clientSecret: 'm-Yoglw09Bf_XtuNzggqj6-y'
+        clientID: '980230234230-43rbramdp31lmb6q649rgbdo277cg8cv.apps.googleusercontent.com',
+        clientSecret: '9-NVcufRwHo4KprRjNPPXbVO'
     },
 };
 
