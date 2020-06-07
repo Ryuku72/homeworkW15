02d5fec2f759985ad7a75174410680f0902016ac
Roz <roz_E4@hotmail.com>
2020-06-07 16:58:35 +0800

    clgs to find what add bookmark is doing

--- public/assets/js/member.js
+++ public/assets/js/member.js
@@ -55,6 +55,8 @@ $(document).ready(function () {
     regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
     let tagInput = $("#bookmarkInput").val().trim();
     let tagVal = tagInput.toLowerCase() 
+    console.log(tagInput);
+    console.log(tagVal);
 
     if ((tagVal.length >= 5 ) && (tagVal.substr(0, 5) != 'http:') && (tagVal.substr(0, 5) != 'https') ) {
        const bookURL = 'http://' + tagVal;
